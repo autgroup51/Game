6359c324890ed0d7610862e06130d93cc2605fa4
Bongscope <victor.zhan.0220@gmail.com>
2016-10-20 18:49:32 +1300

    partial highscore implemented
    score GUI
    code to update score
    cant destroy enemy
    bullets keep on pushing player( lol )

---
+++ New Unity Project/Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnContact : MonoBehaviour {
+
+    private GameManager gameManager;
+    public int score;
+
+    //Find game manager otherwise reference game manager
+	void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameManager");
+        if (gameControllerObject != null)
+        {
+            gameManager = gameManager.GetComponent<GameManager>();
+        }
+        if (gameManager == null)
+        {
+            Debug.Log("Cannot find 'GameManager' script");
+        }
+    }
+
+    //when other touchs this object and is of tag trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Weapon")
+        {
+            gameManager.updateScore(score);
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
